6ccdba09d85dde0ac6621fe427bfa4ccdb7cb73d
Lubos Belak <belak.lubos@gmail.com>
2018-11-28 21:41:38 +0100

    Fake the grid gap by linear-gradient

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const $ = element => document.querySelector(element);
 const $$ = elements => [...document.querySelectorAll(elements)];
 
-const fillTheBlackness = () => {
+const computeOffsets = () => {
 	const last = $('section:last-child');
-	const { style } = document.body;
+	const { style } = document.documentElement;
 	const { left, width } = last.getBoundingClientRect()
 	const offset = Math.ceil(left + width);
 
-	return (
-		'setProperty' in style &&
+	if ('setProperty' in style) {
+		style.setProperty('--area-width', `${width}px`);
 		style.setProperty('--offset', `${offset}px`)
-	);
+	}
 }
 
-window.onload = fillTheBlackness();
-window.addEventListener('resize', fillTheBlackness);
+window.onload = computeOffsets();
+window.addEventListener('resize', computeOffsets);
