3c765e635f982aff41354c5a97b333b7cc4c6091
Lubos Belak <belak.lubos@gmail.com>
2018-11-28 18:39:56 +0100

    FillTheBlackness()

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const $ = element => document.querySelector(element);
 const $$ = elements => [...document.querySelectorAll(elements)];
-const main = $('main');
+
+const fillTheBlackness = () => {
+	const last = $('section:last-child');
+	const { style } = document.body;
+	const { left, width } = last.getBoundingClientRect()
+	const offset = Math.ceil(left + width);
+
+	return (
+		'setProperty' in style &&
+		style.setProperty('--offset', `${offset}px`)
+	);
+}
+
+window.onload = fillTheBlackness();
+window.addEventListener('resize', fillTheBlackness);
