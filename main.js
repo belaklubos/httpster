60864e9510c31f6089aeb0189a55177bf339b42d
Lubos Belak <belak.lubos@gmail.com>
2018-11-28 18:19:58 +0100

    Pump up the font sizes

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
 const $ = element => document.querySelector(element);
 const $$ = elements => [...document.querySelectorAll(elements)];
 const main = $('main');
-
-const path = document.querySelector('.waves__path');
-const length = path.getTotalLength();
-console.log({ length });
