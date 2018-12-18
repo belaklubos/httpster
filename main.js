0cf69b65f0f4ff948259e9ffbafc4fef6735bda2
Lubos Belak <belak.lubos@gmail.com>
2018-12-18 19:20:41 +0100

    Set a delays for the animations dynamically

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const $ = element => document.querySelector(element);
 const $$ = elements => [...document.querySelectorAll(elements)];
+const chill = seconds => new Promise(res => setTimeout(res, seconds * 1000));
 const getSVGPathLength = path => $(path) && $(path).getTotalLength();
 const isCSSVariablesSupported = () => 'setProperty' in document.documentElement.style;
 const setCSSCustomProperty = (prop, val) => isCSSVariablesSupported &&
 	document.documentElement.style.setProperty(prop, val);
-const waitASec = (sec = 1) => new Promise(res => setTimeout(res, sec * 1000));
 
 const computeOffset = () => {
 	const last = $('section:last-child');
@@ -15,14 +15,22 @@ const computeOffset = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
 	const color = getComputedStyle(document.documentElement).getPropertyValue('--secondary-color');
-	$$('[line]').map(line => line.style.stroke = color);
-	$$('[wave]').map(wave => wave.style.stroke = color);
-
-	const hellos = $$('[hello]');
-	$('[hello]:last-of-type').addEventListener('animationend', () => {
-		hellos.map((hello, index) => {
-			hello.removeAttribute('show');
-			waitASec().then(() => hello.setAttribute('show', true));
+	const hells = $$('[hell]');
+	const lines = $$('[line]');
+	const waves = $$('[wave]');
+	
+	hells.map(({ style }, index) => style.animationDelay = `${1 + 0.1 * index}s`);
+	lines.map(line => line.style.stroke = color);
+	waves.map(({ style }, index) => {
+		style.stroke = color;
+		style.animationDelay = `${1 + 0.1 * index}s`
+	});
+	
+	$('[hell]:last-of-type').addEventListener('animationend', () => {
+		hells.map(async (hell) => {
+			hell.removeAttribute('show');
+			await chill(1);
+			hell.setAttribute('show', true);
 		});
 	});
 
